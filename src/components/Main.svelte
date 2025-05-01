4eae042cdaf85065ab4cec1fc7f0b24af6cb513a
lenovo <orangestickfigure123@gmail.com>
2025-04-30 21:20:53 -0400

    changed landing page a bit

--- src/components/Main.svelte
+++ src/components/Main.svelte
@@ -3,14 +3,18 @@
 </script>
 
 <main class="flex flex-col flex-1 p-4">
-    <section id="introPage" class="grid grid-cols-1 lg:grid-cols-2 gap-10 py-8 sm:py-14">
+    <section id="introPage" class=" gap-10 py-8 sm:py-14">
         <div class="flex flex-col lg:justify-center text-center lg:text-left gap-6 md:gap-8 lg:gap-10">
             <h2 class="font-semibold text-4xl sm:text 5xl md:text-7xl">
                 Hi! I'm <span class="poppins text-cyan-500"> <a class="text-cyan-500 hover:text-fuchsia-500" href="/about">Justin</a></span>!
             </h2>
             <p class="text-base sm:text-lg md:text-xl">
                 I'm a Grade 11 student from <span class="text-cyan-200">Toronto, Canada</span>  and I go to <span class="text-cyan-200">William Lyon Mackenzie C.I.</span> <br/>
-                I have made a variety of projects, some of which you can find <a href="/projects" class="duration-200 hover:text-fuchsia-500 text-cyan-500"><u>here</u></a>.
+                I have made a variety of tutorials, some of which you can find <a href="/tutorials" class="duration-200 hover:text-fuchsia-500 text-cyan-500"><u>here</u></a>.
+                <br/>
+                I also have some projects that I've made, a few of which you can find <a href="/projects" class="duration-200 hover:text-fuchsia-500 text-cyan-500"><u>here</u></a>.
+                <br/>
+                Or, you can read more <a href="/about" class="duration-200 hover:text-fuchsia-500 text-cyan-500"><u>about me</u></a>!
             </p>
         </div>
     </section>
